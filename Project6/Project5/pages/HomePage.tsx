98ebc7d852ac5dc5b665acc22b712266933bba1b
Your Name <biswastatay73@gmail.com>
2025-08-23 19:29:49 +0530

    Refactor HomePage component: add user and items interfaces, implement state management, and prepare for TextInput integration

--- Project6/Project5/pages/HomePage.tsx
+++ Project6/Project5/pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useRef, useState } from 'react'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamsList } from '../navigation/AppNavigation'
 
@@ -7,16 +7,44 @@ interface HomeScreenProps {
     navigation: NativeStackNavigationProp<RootStackParamsList, 'HomePage'>;
 }
 
+interface User {
+    name: string,
+    age: number,
+    email: string
+}
+
+interface Items {
+    title: string,
+    price: number,
+    id: string
+}
+
 const HomePage = ({ navigation }: HomeScreenProps) => {
+
+    const [count, setCount] = useState<number>(0);
+    const [name, setName] = useState<string>('')
+    const [isLoaded, setIsLoaded] = useState<boolean>(false)
+    const [user, setUser] = useState<User>({ name: '', age: 0, email: '' })
+    const [items, setItems] = useState<Items[]>([])
+    const [details, setDetails] = useState<null>(null)
+
+    const ref = useRef<TextInput>(null)
+
     return (
         <View style={styles.container}>
             <Text>HomePage</Text>
-            <View>
+            {/* <View>
                 <Button title='Profile Page' onPress={() => navigation.navigate('ProfilePage', {
                     name: "Dwaipayan",
                     email: 'abc@gmail.com'
                 })} />
-            </View>
+            </View> */}
+
+            <TextInput 
+             ref={ref}
+
+            />    
+
         </View>
     )
 }
